727df063359500a5da8b83426b2b3b01c4336688
Debrah Muinde <debbynav645@gmail.com>
2024-10-10 09:11:21 +0300

    Updates index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+function removeLastCat() {
+    const newCats = [...cats];
+    newCats.splice(-1, 1)
+    return newCats;
+}
+
+function removeFirstCat() {
+    const newCats = [...cats]
+    newCats.splice(0,1)
+    return newCats;
+}
